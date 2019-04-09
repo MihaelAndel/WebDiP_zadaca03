21f2b61ec1bfe7c685d6049669b9492b314c4624
Mihael Anđel <andel.mihael@gmail.com>
2019-04-09 18:17:39 +0200

    Dodane funkcionalnosti DataTables
    Dodana odgovarajuća js datoteka. Ažurirana tablica na popis.html kako bi se ispravne mogle prikazivati DataTables funkcionalnosti.

---
+++ javascript/mandel_jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#tablica-popis').DataTable({
+        //"pageLength": 3,
+        //dom: 'Bfrtip',
+        lengthMenu: [
+            [2, 3, 5, 10, 25],
+            ['2', '3', '5', '10', '25']
+        ],
+        buttons: [
+            'pageLength'
+        ]
+    });
+})
