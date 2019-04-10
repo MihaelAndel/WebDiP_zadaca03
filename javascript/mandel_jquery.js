d71e8a0dd7521ed306001c1b607b13637d6ec290
Mihael Anđel <andel.mihael@gmail.com>
2019-04-10 22:16:58 +0200

    Multimedija i posljednja izmjena

--- javascript/mandel_jquery.js
+++ javascript/mandel_jquery.js
@@ -1,16 +1,34 @@
 $(function () {
-    $('#tablica-popis').DataTable({
-        //"pageLength": 3,
-        //dom: 'Bfrtip',
-        lengthMenu: [
-            [2, 3, 5, 10, 25],
-            ['2', '3', '5', '10', '25']
-        ],
-        buttons: [
-            'pageLength'
-        ]
-    });
-})
+
+    if ($("body").attr("id") === "popis") {
+
+        $('#tablica-popis').DataTable({
+            //"pageLength": 3,
+            //dom: 'Bfrtip',
+            lengthMenu: [
+                [2, 3, 5, 10, 25],
+                ['2', '3', '5', '10', '25']
+            ],
+            buttons: [
+                'pageLength'
+            ]
+        });
+    }
+
+    if ($("body").attr("id") === "multimedija") {
+
+        $("#element1").hover(function () {
+            $("#element1").animate({
+                width: "50%"
+            });
+        }, function () {
+            $("#element1").animate({
+                width: "100%"
+            });
+        });
+    }
+
+});
 
 
 //^(\w|(\w\.\w)|\w\.)+@(?=\w*\.)(\w|(\.\w\w)|(\w\.\w\w))+$ regex za e-mail
